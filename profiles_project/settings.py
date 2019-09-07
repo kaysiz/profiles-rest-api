4d692e359491c4b89749ba1b96e0a712e9d169fa
Kudakwashe Siziva <ksiziva@gmail.com>
2019-09-07 20:48:04 +0200

    setup postgresql locally

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'profiles_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'profile-rest-api',
+        'NAME': 'profiles-rest-api',
         'USER': 'kaysiz',
         'PASSWORD': '123456',
         'HOST': '',
